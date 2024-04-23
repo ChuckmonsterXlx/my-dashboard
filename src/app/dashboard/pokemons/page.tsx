86a58b5f809e0b7abfc6843eedabf19d45119a1b
ChuckmonsterXlx <alfonso_309@hotmail.com>
2024-04-23 15:14:50 -0700

    interfaces were added and pokemons are rendered

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,18 +1,40 @@
-const getPokemos = async (limit: number = 20, offset: number = 0) => {
-  const data = await fetch(
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
+
+const getPokemos = async (
+  limit: number = 500,
+  offset: number = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
   ).then((res) => res.json());
 
-  return data;
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!,
+    name: pokemon.name,
+  }));
+
+  return pokemons;
 };
 
 export default async function PokemonsPage() {
   const pokemons = await getPokemos();
 
   return (
-    <>
+    <div className="flex flex-col">
       <h1>Pokemons Page</h1>
-      {JSON.stringify(pokemons)}
-    </>
+
+      <div className="flex flex-wrap items-center justify-center gap-10">
+        {pokemons.map((pokemon) => (
+          <Image
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+            width={100}
+            height={100}
+            alt={pokemon.name}
+            key={pokemon.id}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
