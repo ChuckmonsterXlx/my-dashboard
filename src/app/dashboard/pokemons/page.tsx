cc2eb6bacbd5b4810bb9410f0891b6abc99dac22
ChuckmonsterXlx <alfonso_309@hotmail.com>
2024-04-23 12:14:45 -0700

    pokeapi added

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,3 +1,18 @@
-export default function PokemonsPage() {
-  return <h1>Pokemons Page</h1>;
+const getPokemos = async (limit: number = 20, offset: number = 0) => {
+  const data = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
+
+  return data;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemos();
+
+  return (
+    <>
+      <h1>Pokemons Page</h1>
+      {JSON.stringify(pokemons)}
+    </>
+  );
 }
