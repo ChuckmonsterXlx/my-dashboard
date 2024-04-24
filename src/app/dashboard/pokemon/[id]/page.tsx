046490ad6e8b6d9b098c39b0b24505f0337062e0
ChuckmonsterXlx <alfonso_309@hotmail.com>
2024-04-24 11:30:38 -0700

    the pokemon information is loaded

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,3 +1,26 @@
-export default function PokemonPage() {
-  return <h1>Pokemon Page</h1>;
+import { Pokemon } from "@/pokemons";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: "force-cache",
+  }).then((res) => res.json());
+
+  return pokemon;
+};
+
+export default async function PokemonPage({ params }: Props) {
+  const pokemon = await getPokemon(params.id);
+
+  return (
+    <>
+      <h1>Pokemon ID: {params.id}</h1>
+      {JSON.stringify(pokemon)}
+    </>
+  );
 }
