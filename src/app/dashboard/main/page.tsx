7cc695eaf84103770cd6faafd9da8f5a5a73c528
ChuckmonsterXlx <alfonso_309@hotmail.com>
2024-04-26 17:46:59 -0700

    simplewidget was added

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,3 +1,14 @@
+import { SimpleWidget } from "@/app/components";
+
 export default function MainPage() {
-  return <h1>Main Page</h1>;
+  return (
+    <div className="p-2">
+      <h1>Dashboard</h1>
+      <span>General information</span>
+
+      <div className="grid grid-cols-4 gap-3 p-2">
+        <SimpleWidget />
+      </div>
+    </div>
+  );
 }
