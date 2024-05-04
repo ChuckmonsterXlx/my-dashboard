3558308c81352e972eae1527c6ac412c2d2d1d87
ChuckmonsterXlx <alfonso_309@hotmail.com>
2024-05-03 17:06:11 -0700

    favorite pokemons persist in the state

--- src/pokemons/components/FavoritePokemons.tsx
+++ src/pokemons/components/FavoritePokemons.tsx
@@ -2,11 +2,28 @@
 
 import { useAppSelector } from "@/store";
 import { PokemonGrid } from "./PokemonGrid";
+import { useState } from "react";
+import { IoHeartOutline } from "react-icons/io5";
 
 export const FavoritePokemons = () => {
-  const favoritePokemons = useAppSelector((state) =>
-    Object.values(state.pokemons)
+  const favoritePokemons = Object.values(
+    useAppSelector((state) => state.pokemons)
   );
 
-  return <PokemonGrid pokemons={favoritePokemons} />;
+  const [pokemons, setPokemons] = useState(favoritePokemons);
+
+  return (
+    <>
+      {pokemons.length ? <PokemonGrid pokemons={pokemons} /> : <NoFavorites />}
+    </>
+  );
+};
+
+export const NoFavorites = () => {
+  return (
+    <div className="flex flex-col h-[50vh]  items-center justify-center">
+      <IoHeartOutline size={100} className="text-red-500" />
+      <span>There are no favorites</span>
+    </div>
+  );
 };
